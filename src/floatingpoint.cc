6a2d62ab00429256c31ed94d2aaac1074f898ea4
kingkybel <kingkybel@users.noreply.github.com>
2019-05-13 16:45:56 +0100

    Update floatingpoint.cc

--- src/floatingpoint.cc
+++ src/floatingpoint.cc
@@ -1,19 +1,34 @@
-///////////////////////////////////////////////////////////////////////
-//
-// File:   floatingpoint.cc
-// Re-create Solaris double_to_decimal, decimal_to_double, ...
-// Created on December 13, 2011, 9:05 AM
-// Author: Dieter Kybelksties
-//
-///////////////////////////////////////////////////////////////////////
+/*
+ * File Name:   floatingpoint.cc
+ * Description: Re-create Solaris double_to_decimal, decimal_to_double, ...
+ *
+ * Copyright (C) 2019 Dieter J Kybelksties
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @date: 2011-12-13
+ * @author: Dieter J Kybelksties
+ */
 
+#include <floatingpoint.h>
 #include <mpfr.h>
+#include <gmp.h>
 #include <gmpxx.h>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <floatingpoint.h>
-#include <string.h>
 #include <map>
 #include <cmath>
 #include <iomanip>
@@ -22,7 +37,6 @@
 namespace util
 {
     using namespace std;
-
     decimal_record::decimal_record(enum fp_class_type fpclass_,
                                    int sign_,
                                    int exponent_,
@@ -40,8 +54,7 @@ namespace util
     }
 
     static const unsigned int defaultNumberOfBits = 128;
-
-    bool operator==(const decimal_record& lhs, const decimal_record& rhs)
+    bool operator ==(const decimal_record& lhs, const decimal_record& rhs)
     {
         return lhs.exponent == rhs.exponent
                 && lhs.fpclass == rhs.fpclass
@@ -50,8 +63,7 @@ namespace util
                 && lhs.sign == rhs.sign
                 && !strcmp(lhs.ds, rhs.ds);
     }
-
-    ostream& operator<<(ostream& os, const decimal_record& dr)
+    ostream& operator <<(ostream& os, const decimal_record& dr)
     {
         os << "decimal_record { ds=" << dr.ds
                 << ", exponent=" << dr.exponent
@@ -68,8 +80,7 @@ namespace util
                 << " }";
         return os;
     }
-
-    ostream& operator<<(ostream& os, const decimal_mode& dm)
+    ostream& operator <<(ostream& os, const decimal_mode& dm)
     {
         os << "decimal_mode {"
                 << (dm.df == floating_form ? "floating_form" : "fixed_form") << ", "
@@ -90,36 +101,29 @@ namespace util
 
 
     // helper operators for mpf's
-
     bool operator<(const mpf_class& lhs, const mpf_class& rhs)
     {
         return cmp(lhs, rhs) < 0;
     }
-
-    bool operator==(const mpf_class& lhs, const mpf_class& rhs)
+    bool operator ==(const mpf_class& lhs, const mpf_class& rhs)
     {
         return cmp(lhs, rhs) == 0;
     }
-
-    bool operator!=(const mpf_class& lhs, const mpf_class& rhs)
+    bool operator !=(const mpf_class& lhs, const mpf_class& rhs)
     {
         return !(lhs == rhs);
     }
-
     bool operator>(const mpf_class& lhs, const mpf_class& rhs)
     {
         return cmp(lhs, rhs) > 0;
     }
-
     bool isNegative(const mpf_class& lhs)
     {
         return sgn(lhs) == -1;
     }
 
-    /**
-     * helper: returns a mpf 10^exponent
-     * accepts negative exponents as well, unlike the mpf-function "pow"
-     */
+    // helper: returns a mpf 10^exponent
+    // accepts negative exponents as well, unlike the mpf-function "pow"
     mpf_class ten_to_the_power_of(int exponent)
     {
         mpf_class reval(0.0, defaultNumberOfBits);
@@ -130,18 +134,14 @@ namespace util
         return reval;
     }
 
-    /**
-     * strip 0's from the front and end of a string
-     */
+    // strip 0's from the front and end of a string
     void remove0(string& str)
     {
         util::strip(str, "0");
     }
 
-    /**
-     * round the multi precision float 'val' using the IEEE rounding standards
-     * and precision passed in through 'pm'
-     */
+    // round the multi precision float 'val' using the IEEE rounding standards
+    // and precision passed in through 'pm'
     mpf_class round(mpf_class val, decimal_mode* pm)
     {
         bool negative = isNegative(val);
@@ -161,7 +161,8 @@ namespace util
                 // 1.234000235e-6
                 // so exponent should be -6
                 stringstream ss;
-                ss << fixed << setprecision(40) << abs_val;
+                mp_exp_t e = 0;
+                ss << fixed << setprecision(40) << abs_val.get_str(e);
                 string s = ss.str();
                 string::size_type firstNotNull = s.find_first_not_of("0.");
                 exponent = (firstNotNull != string::npos) ? (1 - int(firstNotNull)) : 1;
@@ -237,60 +238,59 @@ namespace util
         return reval;
     }
 
-    /**
-     * DESCRIPTION (from Sun man-pages)
-     *      The floating_to_decimal functions convert the floating-point
-     *      value  at  *px  into  a decimal record at *pd, observing the
-     *      modes specified in *pm and setting exceptions  in  *ps.   If
-     *      there are no IEEE exceptions, *ps will be zero.
-     *
-     *      If *px is zero, infinity, or NaN,  then  only  pd->sign  and
-     *      pd->fpclass  are  set. Otherwise pd->exponent and pd->ds are
-     *      also set so that
-     *
-     *      (sig)*(pd->ds)*10**(pd->exponent)
-     *
-     *      is a correctly rounded approximation to *px, where sig is +1
-     *      or  -1,  depending upon whether pd->sign is  0 or -1. pd->ds
-     *      has at least one and no  more  than  DECIMAL_STRING_LENGTH-1
-     *      significant  digits  because  one  character is used to ter-
-     *      minate the string with a null.
-     *
-     *      pd->ds is correctly rounded according to the  IEEE  rounding
-     *      modes  in  pm->rd.  *ps has fp_inexact set if the result was
-     *      inexact, and has fp_overflow set if the string  result  does
-     *      not    fit    in    pd->ds   because   of   the   limitation
-     *      DECIMAL_STRING_LENGTH.
-     *
-     *      If pm->df ==  floating_form,  then  pd->ds  always  contains
-     *      pm->ndigits  significant  digits.   Thus if *px == 12.34 and
-     *      pm->ndigits == 8, then  pd->ds  will  contain  12340000  and
-     *      pd->exponent will contain -6.
-     *
-     *      If pm->df == fixed_form  and  pm->ndigits  >=  0,  then  the
-     *      decimal  value is rounded at pm->ndigits digits to the right
-     *      of the decimal point. For  example,  if  *px  ==  12.34  and
-     *      pm->ndigits   ==   1,  then  pd->ds  will  contain  123  and
-     *      pd->exponent will be set to -1.
-     *
-     *      If pm->df == fixed_form and pm->ndigits< 0, then the decimal
-     *      value  is  rounded at -pm->ndigits digits to the left of the
-     *      decimal point, and pd->ds is padded with trailing  zeros  up
-     *      to the decimal point. For example, if *px == 12.34 and pm->n
-     *      digits == -1, then pd->ds will contain 10  and  pd->exponent
-     *      will be set to 0.
-     *
-     *      When pm->df == fixed_form and the value to be  converted  is
-     *      large  enough  that the  resulting string would contain more
-     *      than DECIMAL_STRING_LENGTH-1 digits, then the string  placed
-     *      in  pd->ds  is  limited  to  exactly DECIMAL_STRING_LENGTH-1
-     *      digits (by moving the place at which the  value  is  rounded
-     *      further  left  if need be), pd->exponent is adjusted accord-
-     *      ingly and the overflow flag is set in *ps.
-     *
-     *      pd->more is not used.
-     *
-     */
+    //
+    // DESCRIPTION (from Sun man-pages)
+    //     The floating_to_decimal functions convert the floating-point
+    //     value  at  *px  into  a decimal record at *pd, observing the
+    //     modes specified in *pm and setting exceptions  in  *ps.   If
+    //     there are no IEEE exceptions, *ps will be zero.
+    //
+    //     If *px is zero, infinity, or NaN,  then  only  pd->sign  and
+    //     pd->fpclass  are  set. Otherwise pd->exponent and pd->ds are
+    //     also set so that
+    //
+    //     (sig)*(pd->ds)*10**(pd->exponent)
+    //
+    //     is a correctly rounded approximation to *px, where sig is +1
+    //     or  -1,  depending upon whether pd->sign is  0 or -1. pd->ds // <DKyb> NGBVal uses 1 as sign
+    //     has at least one and no  more  than  DECIMAL_STRING_LENGTH-1
+    //     significant  digits  because  one  character is used to ter-
+    //     minate the string with a null.
+    //
+    //     pd->ds is correctly rounded according to the  IEEE  rounding
+    //     modes  in  pm->rd.  *ps has fp_inexact set if the result was
+    //     inexact, and has fp_overflow set if the string  result  does
+    //     not    fit    in    pd->ds   because   of   the   limitation
+    //     DECIMAL_STRING_LENGTH.
+    //
+    //     If pm->df ==  floating_form,  then  pd->ds  always  contains
+    //     pm->ndigits  significant  digits.   Thus if *px == 12.34 and
+    //     pm->ndigits == 8, then  pd->ds  will  contain  12340000  and
+    //     pd->exponent will contain -6.
+    //
+    //     If pm->df == fixed_form  and  pm->ndigits  >=  0,  then  the
+    //     decimal  value is rounded at pm->ndigits digits to the right
+    //     of the decimal point. For  example,  if  *px  ==  12.34  and
+    //     pm->ndigits   ==   1,  then  pd->ds  will  contain  123  and
+    //     pd->exponent will be set to -1.
+    //
+    //     If pm->df == fixed_form and pm->ndigits< 0, then the decimal
+    //     value  is  rounded at -pm->ndigits digits to the left of the
+    //     decimal point, and pd->ds is padded with trailing  zeros  up
+    //     to the decimal point. For example, if *px == 12.34 and pm->n
+    //     digits == -1, then pd->ds will contain 10  and  pd->exponent
+    //     will be set to 0.
+    //
+    //     When pm->df == fixed_form and the value to be  converted  is
+    //     large  enough  that the  resulting string would contain more
+    //     than DECIMAL_STRING_LENGTH-1 digits, then the string  placed
+    //     in  pd->ds  is  limited  to  exactly DECIMAL_STRING_LENGTH-1
+    //     digits (by moving the place at which the  value  is  rounded
+    //     further  left  if need be), pd->exponent is adjusted accord-
+    //     ingly and the overflow flag is set in *ps.
+    //
+    //     pd->more is not used.
+    //
     void quadruple_to_decimal(quadruple *px,
                               decimal_mode *pm,
                               decimal_record *pd,
@@ -330,9 +330,9 @@ namespace util
             string::size_type pos = valStr.find_first_of(".");
 
             val = round(val, pm);
-
+            mp_exp_t e = 0;
             ss.str("");
-            ss << fixed << setprecision(100) << val;
+            ss << fixed << setprecision(100) << val.get_str(e);
             valStr = ss.str();
 
             if (valStr[0] == '-')
@@ -355,7 +355,7 @@ namespace util
                 valStr = "0" + valStr;
             else if (valStr[valStr.size() - 1] == '.')
                 valStr += "0";
-
+            
             // now we have a "normalized string"
             // "0.0", "1.0", "2.0", "13413.0"
             // "0.2341", "6536.2352", ...
@@ -407,40 +407,40 @@ namespace util
         }
     }
 
-    /**
-     * DESCRIPTION (from Sun man-pages)
-     *      These  functions  convert  the  decimal  record  *pd  to   a
-     *      floating-point  value  *px  observing the rounding direction
-     *      specified in *pm and setting *ps to  reflect  any  floating-
-     *      point exceptions that occur.
-     *
-     *      When  pd->fpclass  is  fp_zero,  fp_infinity,  fp_quiet,  or
-     *      fp_signaling,  *px is set to zero, infinity, a quiet NaN, or
-     *      a signaling NaN, respectively, with the  sign  indicated  by
-     *      pd->sign. All other fields in *pd are ignored.
-     *
-     *      When pd->fpclass is fp_normal or fp_subnormal,  pd->ds  must
-     *      contain a null-terminated string of one or more ASCII digits
-     *      representing a non-zero integer m, and pd->ndigits  must  be
-     *      equal  to  the  length  of this string. Then *px is set to a
-     *      correctly rounded approximation to
-     *
-     *      -1**(pd->sign) * m * 10**(pd->exponent)
-     *
-     *      pd->more can be set to a non-zero  value  to  indicate  that
-     *      insignificant  trailing  digits were omitted from pd->ds. In
-     *      this case, m is replaced by m  +  delta  in  the  expression
-     *      above, where delta is some tiny positive fraction.
-     *
-     *      The converted value is rounded  according  to  the  rounding
-     *      direction  specified  in  pm->rd. pm->df and pm->ndigits are
-     *      not used.
-     *
-     *      On exit, *ps contains a bitwise OR of flags corresponding to
-     *      any floating-point exceptions that occurred. The only possi-
-     *      ble exceptions are underflow, overflow, and inexact.  If  no
-     *      floating-point exceptions occurred, *ps is set to zero.
-     */
+    //
+    // DESCRIPTION (from Sun man-pages)
+    //     These  functions  convert  the  decimal  record  *pd  to   a
+    //     floating-point  value  *px  observing the rounding direction
+    //     specified in *pm and setting *ps to  reflect  any  floating-
+    //     point exceptions that occur.
+    //
+    //     When  pd->fpclass  is  fp_zero,  fp_infinity,  fp_quiet,  or
+    //     fp_signaling,  *px is set to zero, infinity, a quiet NaN, or
+    //     a signaling NaN, respectively, with the  sign  indicated  by
+    //     pd->sign. All other fields in *pd are ignored.
+    //
+    //     When pd->fpclass is fp_normal or fp_subnormal,  pd->ds  must
+    //     contain a null-terminated string of one or more ASCII digits
+    //     representing a non-zero integer m, and pd->ndigits  must  be
+    //     equal  to  the  length  of this string. Then *px is set to a
+    //     correctly rounded approximation to
+    //
+    //     -1**(pd->sign) * m * 10**(pd->exponent)
+    //
+    //     pd->more can be set to a non-zero  value  to  indicate  that
+    //     insignificant  trailing  digits were omitted from pd->ds. In
+    //     this case, m is replaced by m  +  delta  in  the  expression
+    //     above, where delta is some tiny positive fraction.
+    //
+    //     The converted value is rounded  according  to  the  rounding
+    //     direction  specified  in  pm->rd. pm->df and pm->ndigits are
+    //     not used.
+    //
+    //     On exit, *ps contains a bitwise OR of flags corresponding to
+    //     any floating-point exceptions that occurred. The only possi-
+    //     ble exceptions are underflow, overflow, and inexact.  If  no
+    //     floating-point exceptions occurred, *ps is set to zero.
+    //
     void decimal_to_quadruple(quadruple *px,
                               decimal_mode *pm,
                               decimal_record *pd,
@@ -473,7 +473,7 @@ namespace util
 
             tmp *= ten_to_the_power_of(pd->exponent);
 
-            // some NGBVal operations set the flag to 1 rather than -1
+            // some NGBVal operations set the flag to 1 rather than -1 
             // so we do a != 0 here
             if (pd->sign != 0)
                 tmp *= -1.0;
@@ -481,10 +481,12 @@ namespace util
             tmp = round(tmp, pm);
 
             stringstream ss;
-            ss << fixed << setprecision(100) << tmp;
+            mp_exp_t e = 0;
+            ss << fixed << setprecision(100) << tmp.get_str(e);
 
             ss >> *px;
         }
     }
+    
 }; // namespace util
 
