dd7e01b411ea68e6dc292fc3734730caef3dda50
kingkybel <kingkybel@users.noreply.github.com>
2019-05-13 16:33:47 +0100

    Create constants.h

---
+++ include/constants.h
@@ -0,0 +1,47 @@
+/*
+ * File Name:   contants.h
+ * Description: some constants
+ *
+ * Copyright (C) 2019 Dieter J Kybelksties
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @date: 2019-05-13
+ * @author: Dieter J Kybelksties
+ */
+
+#ifndef NS_UTIL_CONTANTS_H_INCLUDED
+#define	NS_UTIL_CONTANTS_H_INCLUDED
+#include <vector>
+
+static std::vector<long long> powTwo = {
+                                        1L << 0, 1L << 1, 1L << 2, 1L << 3, 1L << 4,
+                                        1L << 5, 1L << 6, 1L << 7, 1L << 8, 1L << 9,
+                                        1L << 10, 1L << 11, 1L << 12, 1L << 13, 1L << 14,
+                                        1L << 15, 1L << 16, 1L << 17, 1L << 18, 1L << 19,
+                                        1L << 20, 1L << 21, 1L << 22, 1L << 23, 1L << 24,
+                                        1L << 25, 1L << 26, 1L << 27, 1L << 28, 1L << 29,
+                                        1L << 30, 1L << 31, 1L << 32, 1L << 33, 1L << 34,
+                                        1L << 35, 1L << 36, 1L << 37, 1L << 38, 1L << 39,
+                                        1L << 40, 1L << 41, 1L << 42, 1L << 43, 1L << 44,
+                                        1L << 45, 1L << 46, 1L << 47, 1L << 48, 1L << 49,
+                                        1L << 50, 1L << 51, 1L << 52, 1L << 53, 1L << 54,
+                                        1L << 55, 1L << 56, 1L << 57, 1L << 58, 1L << 59,
+                                        1L << 60, 1L << 61, 1L << 62, 1L << 63
+};
+
+
+#endif	// NS_UTIL_CONTANTS_H_INCLUDED
+
