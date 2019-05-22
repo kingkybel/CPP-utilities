b4859ac1cb815024d22c6436901ee44ffb72717b
kingkybel <kingkybel@users.noreply.github.com>
2019-05-22 16:30:45 +0100

    Update iosutil.h

--- include/iosutil.h
+++ include/iosutil.h
@@ -18,13 +18,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * @date: 2014-01-28
+ * @date: 2019-05-22
  * @author: Dieter J Kybelksties
  */
 
 #ifndef NS_UTIL_IOSUTIL_H_INCLUDED
 #define NS_UTIL_IOSUTIL_H_INCLUDED
 #include <iostream>
+#include <iomanip>
+#include <ios>
 #include <map>
 
 namespace util
@@ -45,7 +47,7 @@ namespace util
         round_open_brace = 0x0100, ///< indicate open intervals with round braces
         symbolic_infinity = 0x0200, ///< indicate full interval with symbolic infinity "oo"
     };
-    const static int stream_mode_xalloc_index = ios_base::xalloc();
+    const static int stream_mode_xalloc_index = std::ios_base::xalloc();
 
     /**
      * Enumeration of complements of stream modes that modify the display of
@@ -62,20 +64,7 @@ namespace util
         no_round_open_brace = ~round_open_brace, ///< don't indicate open intervals with round braces
         no_symbolic_infinity = ~symbolic_infinity, ///< don't indicate full interval with symbolic infinity "oo"
     };
-    const static int stream_mode_complement_xalloc_index = ios_base::xalloc();
-
-    /**
-     * Enumeration of modes that contains combinations of stream_mode values.
-     * These are NOT complement-able.
-     */
-    enum stream_mode_aggregate : long
-    {
-        reset_stream_mode = 0x0000, ///< blank out all and revert to system defaults
-        pure = alpha_bool | hex_char, ///< simple scannable format combination
-        standard = alpha_bool | short_float | round_open_brace, ///< standard format combination
-        safe = squoted_char | hex_char | dquoted_string | dquoted_date | alpha_bool ///< more complex combination
-    };
-    const static int stream_mode_aggregate_xalloc_index = ios_base::xalloc();
+    const static int stream_mode_complement_xalloc_index = std::ios_base::xalloc();
 
     /**
      * Enumeration of modifiers that can only have one alternative, so bit-operations
@@ -89,28 +78,44 @@ namespace util
         scientific_float = 0x0003 ///< display floating point values in scientific format
 
     };
-    const static int stream_mode_alternatives_xalloc_index = ios_base::xalloc();
+    const static int stream_mode_alternatives_xalloc_index = std::ios_base::xalloc();
 
-    std::ostream& operator<<(std::ostream& os, stream_mode sm)
+    /**
+     * Enumeration of modes that contains combinations of stream_mode values.
+     * These are NOT complement-able.
+     */
+    enum stream_mode_aggregate : long
     {
-        os.iword(util::stream_mode) |= sm;
+        reset_stream_mode = 0x0000, ///< blank out all and revert to system defaults
+        pure = alpha_bool | hex_char, ///< simple scannable format combination
+        standard = alpha_bool | short_float | round_open_brace, ///< standard format combination
+        safe = squoted_char | hex_char | dquoted_string | dquoted_date | alpha_bool ///< more complex combination
+    };
+    const static int stream_mode_aggregate_xalloc_index = std::ios_base::xalloc();
+
+    inline std::ostream& operator<<(std::ostream& os, stream_mode sm)
+    {
+        os.iword(util::stream_mode_xalloc_index) |= sm;
         return os;
     }
 
-    std::ostream& operator<<(std::ostream& os, stream_mode_complement sm)
+    inline std::ostream& operator<<(std::ostream& os, stream_mode_complement sm)
     {
-        os.iword(util::stream_mode) &= sm;
+        os.iword(util::stream_mode_complement_xalloc_index) &= sm;
         return os;
     }
 
-    std::ostream& operator<<(std::ostream& os, stream_mode_alternatives sm)
+    inline std::ostream& operator<<(std::ostream& os, stream_mode_alternatives sm)
     {
-
+        long x = (long) sm;
+        std::cout << "stream_mode_alternatives=" << x << std::endl;
+        os.iword(util::stream_mode_alternatives_xalloc_index) = sm;
+        return os;
     }
 
-    std::ostream& operator<<(std::ostream& os, stream_mode_aggregate sm)
+    inline std::ostream& operator<<(std::ostream& os, stream_mode_aggregate sm)
     {
-        os.iword(util::stream_mode) = sm;
+        os.iword(util::stream_mode_aggregate_xalloc_index) = sm;
         return os;
     }
 
@@ -118,47 +123,111 @@ namespace util
      * separate the stream formatting of the util-library from the the
      * formatting of objects outside
      */
-    const static int backup_fmtflags_xalloc_index = ios_base::xalloc();
+    const static int backup_fmtflags_xalloc_index = std::ios_base::xalloc();
 
-    template<std::ostream* pOS = 0 >
+    struct floatFmt
+    {
+
+        floatFmt()
+        : isScientific_(true)
+        {
+        }
+
+        floatFmt(size_t width,
+                 size_t precision = 5,
+                 char fill = '0',
+                 bool isFixed = false)
+        : width_(width)
+        , precision_(precision)
+        , fill_(fill)
+        , isFixed_(isFixed)
+        , isScientific_(false)
+        {
+        }
+
+        size_t width_;
+        size_t precision_;
+        char fill_;
+        bool isFixed_;
+        bool isScientific_;
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const floatFmt& fmt)
+    {
+        if (fmt.isScientific_)
+            os << std::ios::scientific;
+        else
+        {
+            os.fill(fmt.fill_);
+            os.width(fmt.width_);
+            if (fmt.isFixed_)
+                os << std::fixed;
+        }
+        return os;
+    }
+
     struct streamModeHandler
     {
-        static size_t ref_count = 0;
+        static std::map<std::ostream*, size_t> ref_count;
+        std::ostream& os;
 
-        streamModeHandler()
+        streamModeHandler(std::ostream& ostr)
+        : os(ostr)
         {
-            if (pOS != 0)
+            std::ios::fmtflags ff(std::ios::fmtflags(0));
+            if (ref_count[&os] == 0)
             {
-                if (ref_count == 0)
-                {
-                    // initialize
-                }
-                // create/increment the ref-counter for this stream
-                ref_count++;
+                // initialize
+
                 // reset the current flags on the stream and save the previous state in
                 // the backup location
-                pOS->iword(util::backup_fmtflags_xalloc_index) = pOS->flags(0);
-
-                // first get the aggregate as a basis
-                long aggMode = pOS->iword(stream_mode_aggregate_xalloc_index);
-
-                // then get alternatives
-                long altMode = pOS->iword(stream_mode_alternatives);
-
-                // remove all from aggregated mode that are flagged as removed
+                os.iword(util::backup_fmtflags_xalloc_index) = os.flags(ff);
 
             }
+            std::cout << "streamModeHandler- ff=" << ff << " stored=" << os.iword(util::backup_fmtflags_xalloc_index) << std::endl;
+            ++(ref_count[&os]);
+
+            // first get the aggregate as a basis
+            long aggMode = os.iword(stream_mode_aggregate_xalloc_index);
+
+            // remove all from aggregated mode that are flagged as removed
+            aggMode &= os.iword(stream_mode_complement_xalloc_index);
+
+            // add all set stream-mode flags
+            aggMode |= os.iword(stream_mode_xalloc_index);
+
+
+            // then get alternatives
+            stream_mode_alternatives altMode = (stream_mode_alternatives) os.iword(stream_mode_alternatives_xalloc_index);
+            if (altMode == scientific_float)
+            {
+                os << floatFmt();
+            }
+            if (altMode == long_float)
+            {
+                os << floatFmt(10, 10);
+            }
+            if (altMode == short_float)
+            {
+                os << floatFmt(10, 10);
+            }
+
         }
+        streamModeHandler(const streamModeHandler&) = delete;
+        streamModeHandler& operator=(const streamModeHandler&) = delete;
+        streamModeHandler(const streamModeHandler&&) = delete;
+        streamModeHandler& operator=(const streamModeHandler&&) = delete;
 
         ~streamModeHandler()
         {
-            if (pOS != 0)
+            if (os != 0)
             {
-                ref_count--;
-                if (ref_count == 0)
+                ref_count[&os]--;
+                std::cout << "~streamModeHandler() ref=" << ref_count[&os] << std::endl;
+                if (ref_count[&os] == 0)
                 {
                     // restore the state of the stream from the backup location
-                    pOS->iword(util::backup_fmtflags_xalloc_index) = pOS->flags(0);
+                    os.flags((std::ios::fmtflags)os.iword(util::backup_fmtflags_xalloc_index));
                 }
             }
         }
