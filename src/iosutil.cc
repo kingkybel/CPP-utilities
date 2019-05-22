043edef36647ec29adfacac019ebb64d53ea79e6
kingkybel <kingkybel@users.noreply.github.com>
2019-05-22 16:34:15 +0100

    Create iosutil.cc

---
+++ src/iosutil.cc
@@ -0,0 +1,28 @@
+/*
+ * File Name:   iosutil.h
+ * Description: IO stream configuration
+ *
+ * Copyright (C) 2019 Dieter J Kybelksties
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @date: 2019-05-22
+ * @author: Dieter J Kybelksties
+ */
+#include "iosutil.h"
+namespace util
+{
+    std::map<std::ostream*, size_t> streamModeHandler::ref_count;
+};
